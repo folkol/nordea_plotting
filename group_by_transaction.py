500fb82d2113d29c684b3f3f51f5d9d43e43ab43
Matte <mattias4@kth.se>
2016-10-02 14:09:24 +0200

    More categories.

--- group_by_transaction.py
+++ group_by_transaction.py
@@ -1,17 +1,31 @@
 import pandas as pd
 from utils import parse_float
+import matplotlib.pyplot as plt
 
 
-def f(x):
-    if "espresso" in x.lower():
-        return "Kaffe"
-    elif "ica supermarket" in x.lower():
+def categorize(x):
+    # if "espresso" in x.lower():
+    #     return "Kaffe"
+    if "ICA SUPERMARKET BROM" in x:
         return "ICA"
-    elif "onlinepizza" in x.lower():
-        return "PIZZA"
-    elif "sbab" in x.lower():
-        return "SBAB"
+    if 'ESPRESSO HOUSE 218' in x:
+        return 'Fika'
+    if 'Omsättning lån 3065 82 24190' in x:
+        return 'Lån'
+    if 'BÖNOR O BLAD MOOD' in x:
+        return 'Kaffe'
+    if 'Autogiro FOLKSAM' in x:
+        return 'Räkningar'
+    if 'SL BROMMAPLAN T BANA' in x:
+        return 'SL'
+    if 'Arbetslöshetska' in x:
+        return 'Räkningar'
+    # elif "ONLINEPIZZA" in x.upper():
+    #     return "PIZZA"
+    # elif "sbab" in x.lower():
+    #     return "SBAB"
     else:
+        print('Could not categorize: ' + x);
         return "Övrigt"
 
 
@@ -22,15 +36,10 @@ data = pd.read_csv(
 )
 
 data = data[data['Belopp'] < 0]  # Only consider withdrawals...
-
-data['Belopp'] = data['Belopp'].map(lambda x: -x)
-data['Kategori'] = data['Transaktion'].map(f)
-
-print(data)
-
+data['Kategori'] = data['Transaktion'].map(categorize)
 data = data.groupby('Kategori')['Belopp'].sum()
 
-data.sort_values(ascending=False).plot(kind='bar', title='Utgifter')
-
-import matplotlib.pyplot as plt
+data.index.name = ''  # TODO: Figure out how to just not plot this instead...
+data.sort_values().plot(kind='bar', title='Utgifter'),
+plt.gca().invert_yaxis()
 plt.show()
